cbc64268e3c89d4e23c8ff98c25bf073310da176
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-19 15:44:21 +0200

    release

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 "use strict"
 
-const app = 
+const app = Vue.createApp({
+    data(){
+        return {
+            messaggio: "FIRST VUE TITLE",
+            para: "WELCOME",
+            user: "STRANGER",
+            centerText: "text-center mt-4",
+            spacingText: "py-3",
+            image: "./img/card-1.png",
+            visibility: "d-none"
+        }
+    },
+    methods: {
+        salutoIniziale(){
+            this.para = this.para + " " + this.user;
+            this.visibility = "d-block"
+        }
+    }
+
+})
+
+app.mount("#app");
